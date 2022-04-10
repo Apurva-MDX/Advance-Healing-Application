b2f809c9c33a334c0d76e8f63f5495206b66da44
Apurva Hingul <apurvahingul123@gmail.com>
2022-04-11 00:01:17 +0100

    I changed the action bar color and hide the title bar using 'MainActivity.java'

--- app/src/main/java/com/example/advancehealing/MainActivity.java
+++ app/src/main/java/com/example/advancehealing/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.advancehealing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); ///This and the line below are responsible for hiding the title bar.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
