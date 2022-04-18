e14927609ce645875eb23ff5b929ac3f7ac44d5e
Apurva Hingul <apurvahingul123@gmail.com>
2022-04-18 18:43:29 +0100

    Digital Games UI with back button navigation is complete.

--- app/src/main/java/com/example/advancehealing/FireworkGame.java
+++ app/src/main/java/com/example/advancehealing/FireworkGame.java
@@ -3,18 +3,32 @@ package com.example.advancehealing;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class FireworkGame extends Fragment {
+public class FireworkGame extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.fragment_firework_game, container, false); //Presents the firework game layout
+        ImageButton button = (ImageButton) view.findViewById(R.id.backBtn); // Finds the 'fidgetBtn' id from NAV1 fragment.
+        button.setOnClickListener(this); //calls the OnClickListener from within the class (line 14).
         return view;
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.backBtn:
+                FragmentTransaction ft2= getFragmentManager().beginTransaction(); //if clicked, it begins the transaction
+                ft2.replace(R.id.container_layout, new NAV1Fragment()); //finds 'container_layout' from MainActivity FrameLayout and replaces with 'FidgetSpinerGame' fragment.
+                ft2.commit(); //commits the changes
+                break;
+    }
+}}
--- app/src/main/java/com/example/advancehealing/VibrationMassagerGame.java
+++ app/src/main/java/com/example/advancehealing/VibrationMassagerGame.java
@@ -1,19 +1,36 @@
 package com.example.advancehealing;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class VibrationMassagerGame extends Fragment {
+public class VibrationMassagerGame extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.fragment_vibration_massager_game, container, false); //Presents the vibration game layout
+        ImageButton button = (ImageButton) view.findViewById(R.id.backBtn); // Finds the 'fidgetBtn' id from NAV1 fragment.
+        button.setOnClickListener(this); //calls the OnClickListener from within the class (line 14).
         return view;
+
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.backBtn:
+                FragmentTransaction ft2= getFragmentManager().beginTransaction(); //if clicked, it begins the transaction
+                ft2.replace(R.id.container_layout, new NAV1Fragment()); //finds 'container_layout' from MainActivity FrameLayout and replaces with 'FidgetSpinerGame' fragment.
+                ft2.commit(); //commits the changes
+                break;
+    }
+}}
