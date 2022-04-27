d4383a2adb27a65d68c1ff3fe0fc5bd5faf571b4
Apurva Hingul <apurvahingul123@gmail.com>
2022-04-27 20:58:26 +0100

    Meditation feature completed! - I improved the background design/layout and added 2 music (.mp3) files to play when the play buttons are pressed. The stop button stops the music, A gif file is used to teach the breathing exercise.

--- app/src/main/java/com/example/advancehealing/NAV2Fragment.java
+++ app/src/main/java/com/example/advancehealing/NAV2Fragment.java
@@ -1,5 +1,6 @@
 package com.example.advancehealing;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,27 +9,47 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 
-public class NAV2Fragment extends Fragment implements View.OnClickListener{
+public class NAV2Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_n_a_v2, container, false);
-        ImageButton button = (ImageButton) view.findViewById(R.id.music2Btn); // Finds the 'music2Btn' id from NAV2 fragment.
-        button.setOnClickListener(this); //calls the OnClickListener from within the class (line 14).
+        ImageButton button = (ImageButton) view.findViewById(R.id.music2Btn);
+        Button button2 = (Button) view.findViewById(R.id.stopmusic2);// Finds the 'stopmusic2' id from NAV2 fragment.
+        ImageButton button3 = (ImageButton) view.findViewById(R.id.music1Btn);
+        Button button4 = (Button) view.findViewById(R.id.stopmusic1);// Finds the 'stopmusic1' id from NAV2 fragment.
+        final MediaPlayer musicPlayer1 = MediaPlayer.create(getActivity(), R.raw.music1);
+        final MediaPlayer musicPlayer2 = MediaPlayer.create(getActivity(), R.raw.music2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                musicPlayer1.start();
+            }
+        }); //calls the OnClickListener from within the class (line 14).
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                musicPlayer1.pause();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                musicPlayer2.start();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                musicPlayer2.pause();
+            }
+        });
         return view;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.music2Btn:
-                FragmentTransaction ft3= getFragmentManager().beginTransaction(); //if clicked, it begins the transaction
-                ft3.replace(R.id.container_layout, new NAV2Fragment2()); //finds 'container_layout' from MainActivity FrameLayout and replaces with 'NAV2Fragment2' fragment.
-                ft3.commit(); //commits the changes
-                break;
     }
-}}
